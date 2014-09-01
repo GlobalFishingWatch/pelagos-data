b77117d2b8d50fa98c118c64866ffdb86795fbf3
Paul Woods <pwoods25443@gmail.com>
2014-09-01 11:20:44 -0400

    Add deploy commandline for vesselinfo upload

--- gce/deploy-gae.sh
+++ gce/deploy-gae.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Clone the repo
+# Clone the repos
 git clone --recursive git@github.com:SkyTruth/pelagos.git
+git clone --recursive git@github.com:SkyTruth/pelagos-data.git
 
 # Initialize GAE virutalenv
 #TODO: Put this into a script in the pelagos repo
@@ -12,3 +13,10 @@ pip install -r pelagos/gae-requirements.txt
 # Deploy
 # Update app.yaml to match project
 gcloud preview app deploy pelagos/gae --project skytruth-pelagos-dev
+
+#Upload vessselinfo (will prompt for password)
+#TODO: direct log files cfreated by uploader to a tempfolder and cleanup afterward
+appcfg.py upload_data --config_file=./pelagos-data/data/vesselinfo/vesselinfo-bulkloader.yaml \
+--filename=./pelagos-data/data/vesselinfo/vessel-info-2014-08-28.csv --kind=VesselInfo \
+--num_threads=4 --url=http://skytruth-pelagos-dev.appspot.com/_ah/remote_api  \
+--rps_limit=500 --email=paul@skytruth.org
