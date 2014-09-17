2e3eff7ffdde1789993fe7dd23f6ecfdf4356e0b
Kevin Wurster <kevin@skytruth.org>
2014-09-17 12:08:58 -0400

    Fixed typos and added a 'gcloud auth login' command to the howto

--- gce/process_raw.sh
+++ gce/process_raw.sh
@@ -19,7 +19,7 @@ bq load --skip_leading_rows=1 --max_bad_records=100 \
 cd /usr/local/src/pelagos-data/
 gunzip -c ./data/sample_ais/processed_1_3_10k_points.csv.gz | \
     ./utils/regionate.py ./data/regions/regions.sqlite | \
-    gzip -c > ~/regionated_1_3_10k_points.jsin.gz
+    gzip -c > ~/regionated_1_3_10k_points.json.gz
 
 # Regionate
 gunzip -c atw2_20k-processed.csv.gz |
@@ -36,15 +36,3 @@ bq load --source_format=NEWLINE_DELIMITED_JSON \
     Scratch.atw2_20k_region_dev2 gs://analyze-data/processed/atw2_20k-region-dev2.json.gz \
     /usr/local/src/pelagos-data/schema/scored-ais-processed-schema-1.4.json
 
-cat > my-new-tileset-name.json <<EOF
-{
-  "name": "Global-20k-2012",
-  "workspace_template": "pelagos-2012-template.json",
-  "params": {
-    "table": "Global_20k_2012.processed_1_4_1"
-  },
-  "generator": "bqtile.TileGenerator"
-}
-EOF
-
-gsutil cp -a public-read my-new-tileset-name.json gs://skytruth-pelagos-redhog/tilesets
