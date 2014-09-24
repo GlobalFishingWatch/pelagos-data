6e626f5fb475361718fe34de0c34704a2f673135
Kevin Wurster <kevin@skytruth.org>
2014-09-24 15:04:37 -0400

    Testing GDAL build script and updated travis.yml

--- gce/build-gdal.sh
+++ gce/build-gdal.sh
@@ -1,18 +1,22 @@
+#!/bin/bash
 
-sudo apt-get install build-essential python-all-dev libsqlite3-dev libspatialite-dev libgeos-dev -y
+# Download dependencies
+sudo apt-get install build-essential python-all-dev libsqlite3-dev libspatialite-dev libgeos-dev libgdal1-dev -y
 
+# Get source
 wget http://download.osgeo.org/gdal/1.11.0/gdal-1.11.0.tar.gz
 tar xvfz gdal-1.11.0.tar.gz
 cd gdal-1.11.0
 
-./configure --with-python --with-spatialite=yes --with-libgeos=yes
-make
+./configure \
+    --with-python \
+    --with-spatialite=YES \
+    --with-libgeos=YES \
+    --with-ogr
+
+# Make on all cores
+make -j `sysctl -n hw.ncpu`
 sudo make install
 
-
-
-export "LD_LIBRARY_PATH=/usr/local/lib"
-
-or
-
- sudo ldconfig ???
+echo "Testing GEOS ..."
+./test-geos.py
