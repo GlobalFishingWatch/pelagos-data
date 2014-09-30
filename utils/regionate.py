d42385d7fba794f66635a266b78a14d8a9adbbc9
Kevin Wurster <kevin@skytruth.org>
2014-09-30 15:35:14 -0400

    Allow list of values for region ID

--- utils/regionate.py
+++ utils/regionate.py
@@ -100,7 +100,9 @@ def regionate (file_in, file_out, arg):
             while feature:
                 field_idx = feature.GetFieldIndex(arg['--attribute'])
                 if field_idx != -1 and feature.GetGeometryRef().Intersects(point):
-                    regionids.append(feature.GetFieldAsString(field_idx))
+                    # TODO: Replace line below with:
+                    #regionids.append(feature.GetFieldAsString(field_idx))
+                    regionids += feature.GetFieldAsString(field_idx).split(',')
                 feature = layer.GetNextFeature()
         row_out = row
         row_out['region'] = regionids
