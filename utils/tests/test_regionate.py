a0fadba57e06013f1ed59a90b43ba011f8e9af2c
Kevin Wurster <kevin@skytruth.org>
2014-10-02 17:12:06 -0400

    New regionate that allows region field mapping

--- utils/tests/test_regionate.py
+++ utils/tests/test_regionate.py
@@ -20,9 +20,13 @@ class RegionateTest(unittest2.TestCase):
         csv_in = self._open_fixture('regionate_input.csv')
         actual_output = StringIO.StringIO()
         expected_output = self._open_fixture('regionate_output_pipa.json')
-        args={'POLY_LAYER': self._get_fixture_path('pipa/pipa.shp'),
-              '--attribute':'regionid',
-              '--layername': None}
+        args = {
+            'POLY_LAYER': self._get_fixture_path('pipa/pipa.shp'),
+            '--attribute': 'regionid',
+            '--layername': None,
+            '--regionid-map': None,
+            '--regionid-mode': 'append'
+        }
         regionate.regionate(csv_in, actual_output, args)
         actual_output.seek(0)
         for expected in expected_output:
@@ -33,9 +37,13 @@ class RegionateTest(unittest2.TestCase):
         csv_in = self._open_fixture('regionate_input.csv')
         actual_output = StringIO.StringIO()
         expected_output = self._open_fixture('regionate_output_ocean.json')
-        args={'POLY_LAYER': self._get_fixture_path('ocean-region-10km.sqlite'),
-              '--attribute': 'regionid',
-              '--layername': 'ocean2'}
+        args = {
+            'POLY_LAYER': self._get_fixture_path('ocean-region-10km.sqlite'),
+            '--attribute': 'regionid',
+            '--layername': 'ocean2',
+            '--regionid-map': None,
+            '--regionid-mode': 'append'
+        }
         regionate.regionate(csv_in, actual_output, args)
         actual_output.seek(0)
         for expected in expected_output:
