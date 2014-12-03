a1e9c4da505a1e259da0503a1e661c17740bef35
Kevin Wurster <kevin@skytruth.org>
2014-12-03 13:11:14 -0500

    Migrated to: https://github.com/SkyTruth/Benthos/

--- bin/getpoint.py
+++
@@ -1,7 +0,0 @@
-import global_measures
-
-r = global_measures.PixelReader('distance-from-port-2km/distance-from-port.tif')
-for lat in xrange(-90, 90):
-    for lng in xrange(-180, 180):
-        val = r.read(lng, lat)
-        print "%s,%s = %s" % (lng, lat, val)
